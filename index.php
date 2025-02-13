7db3927454acd803985b0c5df0c38f927e01a414
clydetimoteo <clydetimoteo@users.noreply.github.com>
2025-02-13 20:02:44 +0800

    Beta 4

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ include 'db_connection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HRMS</title>
-    
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <style>
         body {
